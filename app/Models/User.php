bebd2a9a7432ee3ddda9e8e99de4c7c4ed0b5917
六指黑侠 <zf18600004319@aliyun.com>
2022-05-24 14:40:31 +0800

    添加了判断邮箱是否验证的方法

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,12 +5,13 @@ namespace App\Models;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
-  use HasApiTokens, HasFactory, Notifiable;
+  use HasApiTokens, HasFactory, Notifiable, MustVerifyEmailTrait;
 
   /**
    * The attributes that are mass assignable.
